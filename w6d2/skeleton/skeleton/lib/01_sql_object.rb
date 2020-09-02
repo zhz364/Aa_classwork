e7553819f4e06314c4b548861d597638b70a3a9d
zhz364 <zhz364@ucsd.edu>
2020-09-01 18:13:16 -0700

    Uploaded metaprogramming project

---
+++ w6d2/skeleton/skeleton/lib/01_sql_object.rb
@@ -0,0 +1,67 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    # ...
+    DBConnection.execute2(<<-SQL)
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+SQL
+    
+  end
+
+  def self.finalize!
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+    #CatToy  cat_toy cat_toys
+    # ...
+  end
+
+  def self.table_name
+    @table_name || self.name.underscore.pluralize
+    # ...
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
